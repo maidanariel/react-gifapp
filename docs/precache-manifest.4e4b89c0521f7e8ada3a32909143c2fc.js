367f61d78cbb5b06101a3c0db7ca5bf00d346763
Ariel Maidana <n.ariel.m@gmail.com>
2020-09-04 09:34:42 -0400

    Docs agregado

---
+++ docs/precache-manifest.4e4b89c0521f7e8ada3a32909143c2fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb310afbb3e086d2e5d1efee7862f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ebfa9004ad611f462a",
+    "url": "/static/css/main.9f3911f6.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ebfa9004ad611f462a",
+    "url": "/static/js/main.a08fad95.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
